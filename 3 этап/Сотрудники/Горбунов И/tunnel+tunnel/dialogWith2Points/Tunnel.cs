5eaf73e9872f71f80b15de09a1c2bf2124e3826d
Igor Gorbunov <give@list.ru>
2013-05-30 15:15:48 +0400

    add tunnel class

---
+++ 3 этап/Сотрудники/Горбунов И/tunnel+tunnel/dialogWith2Points/Tunnel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NXOpen;
+
+/// <summary>
+/// Класс содержащий отверстие для базирования.
+/// </summary>
+public class Tunnel
+{
+    
+    Face face;
+
+    /// <summary>
+    /// Инициализирует новый экземпляр класса отверстия для базирования для данной грани.
+    /// </summary>
+    /// <param name="face"></param>
+    public Tunnel(Face face)
+    {
+        this.face = face;
+    }
+}
+
